7803c28d1e9de695f2deabc9faddd539353ba2c8
Rick Sladkey <rick.sladkey@gmail.com>
2010-12-20 22:06:40 +0000

    Fix sample data in Expression Blend.

---
+++ Solitaire/View/CardTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using Spider.Solitaire.ViewModel;
+
+namespace Spider.Solitaire.View
+{
+    public class CardTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate EmptySpaceTemplate { get; set; }
+        public DataTemplate DownCardTemplate { get; set; }
+        public DataTemplate UpCardTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var cardViewModel = item as CardViewModel;
+            switch (cardViewModel.CardType)
+            {
+                case CardType.EmptySpace: return EmptySpaceTemplate;
+                case CardType.Down: return DownCardTemplate;
+                case CardType.Up: return UpCardTemplate;
+            }
+            return null;
+        }
+    }
+}
